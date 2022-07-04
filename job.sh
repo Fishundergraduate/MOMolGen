a67818b4a4a157ecf69148ed4478859c2ddf3bae
suzuki-tsubame <suzuki.t.dq@m.titech.ac.jp>
2022-07-04 22:20:30 +0900

    To Dev MOMolGen

--- job.sh
+++ job.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 #$ -cwd
-#$ -l f_node=2
-#$ -l h_rt=00:10:00
+#$ -l h_node=2
+#$ -l h_rt=0:10:00
 
-module load python/3.9.2 cuda/11.6.1 cudnn/8.3
+module load python/3.9.2 cuda/11.2.146 cudnn/8.1 nccl/2.8.4 tensorflow/2.8.0
 
 #bash init.sh
-source .venv/bin/activate
-python train_RNN/train_RNN.py && $(curl -X POST https://maker.ifttt.com/trigger/JOB_FINISH/with/key/bD0xPz0Ajd2SWn6JVoww3w?value1=`date +%H%M%S`; true) || curl -X POST https://maker.ifttt.com/trigger/JOB_FINISH/with/key/bD0xPz0Ajd2SWn6JVoww3w?value1=`ERROR`
-deactivate
+#source .venv/bin/activate
+python train_RNN/train_RNN.py # && $(curl -X POST https://maker.ifttt.com/trigger/JOB_FINISH/with/key/bD0xPz0Ajd2SWn6JVoww3w?value1=`date +%H%M%S`; true) || curl -X POST https://maker.ifttt.com/trigger/JOB_FINISH/with/key/bD0xPz0Ajd2SWn6JVoww3w?value1=`ERROR`
+#deactivate
 echo finish
---
+++ jobCONF.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#$ -cwd
+#$ -l s_gpu=2
+#$ -l h_rt=0:10:00
+
+ipconfig
