8dfd7b3b5e8067b4cb67beb39e87327970043b13
suzuki-tsubame <suzuki.t.dq@m.titech.ac.jp>
2022-10-10 01:00:56 +0900

    Update JobShellScript

---
+++ job6submit_ligand_design6.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+mkdir log$(($1*24))h_ChemTS_subj
+for i in `seq 21 21`
+do
+    cp -pr data_test_2 log$(($1*24))h_ChemTS_subj
+    qsub -g tga-science ligand_design.$(($i)).job.sh
+    echo submitted $i
+done
